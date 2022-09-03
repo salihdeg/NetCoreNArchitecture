7c13066254a7016a13cd7bda88e87f9975091603
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-03 18:17:05 +0300

    Service Registration Configuration Created for Persistence Layer.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Persistence.Context;
+using Kodlama.io.Devs.Persistence.Repositories.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kodlama.io.Devs.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options=> options.UseSqlServer(configuration.GetConnectionString("KodlamaIoDevsConnectionString")));
+            services.AddScoped<IProgrammingLanguageRepository, EFProgrammingLanguageRepository>();
+            return services;
+        }
+    }
+}
