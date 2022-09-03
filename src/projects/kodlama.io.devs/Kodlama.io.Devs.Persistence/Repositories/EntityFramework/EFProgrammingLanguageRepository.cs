371ea53be4ca734a325e8e1e113b9c61da24976b
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-03 18:14:34 +0300

    Entity Repository Created for Entity Framework

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Repositories/EntityFramework/EFProgrammingLanguageRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using Kodlama.io.Devs.Persistence.Context;
+
+namespace Kodlama.io.Devs.Persistence.Repositories.EntityFramework
+{
+    public class EFProgrammingLanguageRepository : EfRepositoryBase<ProgrammingLanguage, BaseDbContext>, IProgrammingLanguageRepository
+    {
+        public EFProgrammingLanguageRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
