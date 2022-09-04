865ef22649326543137a2ffc484d767f195f74b0
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-04 14:55:19 +0300

    New Entity Pageable Model Created for Queries.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Models/ProgrammingLanguageListModel.cs
@@ -0,0 +1,10 @@
+﻿using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Dtos;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Models
+{
+    public class ProgrammingLanguageListModel : BasePageableModel
+    {
+        public IList<ProgrammingLanguageListDto> Items { get; set; }
+    }
+}
