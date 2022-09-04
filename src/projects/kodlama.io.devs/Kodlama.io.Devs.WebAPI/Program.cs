c2556c3ad79d7ddeb383e0b1a6dbb382ee4587bc
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-04 14:49:47 +0300

    Refactoring.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Program.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Program.cs
@@ -6,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
+
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
