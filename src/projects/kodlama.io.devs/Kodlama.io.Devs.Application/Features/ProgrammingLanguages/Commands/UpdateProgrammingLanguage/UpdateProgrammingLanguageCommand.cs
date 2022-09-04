bde76bc0b6d6622ba99b99ed76763cea344f62a1
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-04 15:13:03 +0300

    New Business Rule Added.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommand.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommand.cs
@@ -28,6 +28,7 @@ namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.Upd
             public async Task<UpdatedProgrammingLanguageDto> Handle(UpdateProgrammingLanguageCommand request, CancellationToken cancellationToken)
             {
                 await _programmingLanguageBusinessRules.ProgrammingLanguageShouldBeExist(request.Id);
+                await _programmingLanguageBusinessRules.ProgrammingLanguageNameCanNotBeDuplicatedWhenInserted(request.Name);
 
                 ProgrammingLanguage mappedProgrammingLanguage = _mapper.Map<ProgrammingLanguage>(request);
                 ProgrammingLanguage updatedProgrammingLanguage = await _programmingLanguageRepository.UpdateAsync(mappedProgrammingLanguage);
