d8fc644acd6e99397cd7e40a4e583bd1c8d46576
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-09 05:00:47 +0300

    Entity Repositories Created by Using EntityFramework Base Repository.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Repositories/EntityFramework/EFProgrammingTechnologyTypeRepositroy.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using Kodlama.io.Devs.Persistence.Context;
+
+namespace Kodlama.io.Devs.Persistence.Repositories.EntityFramework
+{
+    public class EFProgrammingTechnologyTypeRepositroy : EfRepositoryBase<ProgrammingTechnologyType, BaseDbContext>, IProgrammingTechnologyTypeRepository
+    {
+        public EFProgrammingTechnologyTypeRepositroy(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
