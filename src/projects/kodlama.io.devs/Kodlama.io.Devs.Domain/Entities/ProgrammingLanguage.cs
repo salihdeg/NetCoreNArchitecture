97ff3c26a2245f0f9fa4c6dcebbfef24b2c52a06
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-09 04:14:18 +0300

    Entity Modified for Join Operations.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.Domain/Entities/ProgrammingLanguage.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Domain/Entities/ProgrammingLanguage.cs
@@ -1,15 +1,11 @@
 ﻿using Core.Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kodlama.io.Devs.Domain.Entities
 {
     public class ProgrammingLanguage : Entity
     {
         public string Name { get; set; }
+        public virtual ICollection<ProgrammingTechnology> ProgrammingTechnologies { get; set; }
 
         public ProgrammingLanguage()
         {
