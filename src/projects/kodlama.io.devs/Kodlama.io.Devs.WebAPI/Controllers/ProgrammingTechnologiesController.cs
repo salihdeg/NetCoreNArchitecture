08d4577d4c6158bdfabc286209925f60c6143ad8
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-09 15:50:44 +0300

    New Api Controllers are Added for New Entities.

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingTechnologiesController.cs
@@ -0,0 +1,61 @@
+﻿using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Dtos;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Commands.CreateProgrammingTechnology;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Commands.DeleteProgrammingTechnology;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Commands.UpdateProgrammingTechnology;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Dtos;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Models;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Queries.GetListProgrammingTechnology;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Queries.GetListProgrammingTechnologyByDynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodlama.io.Devs.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingTechnologiesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingTechnologyCommand createProgrammingTechnologyCommand)
+        {
+            CreatedProgrammingTechnologyDto result = await Mediator.Send(createProgrammingTechnologyCommand);
+            return Created("", result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateProgrammingTechnologyCommand updateProgrammingTechnologyCommand)
+        {
+            UpdatedProgrammingTechnologyDto result = await Mediator.Send(updateProgrammingTechnologyCommand);
+            return Ok(result);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete([FromRoute] DeleteProgrammingTechnologyCommand deleteProgrammingTechnologyCommand)
+        {
+            DeletedProgrammingTechnologyDto result = await Mediator.Send(deleteProgrammingTechnologyCommand);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingTechnologyQuery getListProgrammingTechnologyQuery = new() { PageRequest = pageRequest };
+            ProgrammingTechnologyListModel result = await Mediator.Send(getListProgrammingTechnologyQuery);
+            return Ok(result);
+        }
+
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListProgrammingTechnologyByDynamicQuery getListProgrammingTechnologyByDynamicQuery = new() { PageRequest = pageRequest, Dynamic = dynamic };
+            ProgrammingTechnologyListModel result = await Mediator.Send(getListProgrammingTechnologyByDynamicQuery);
+            return Ok(result);
+        }
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.WebAPI/Controllers/ProgrammingTechnologyTypesController.cs
@@ -0,0 +1,46 @@
+﻿using Core.Application.Requests;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Commands.CreateProgrammingTechnologyType;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Commands.DeleteProgrammingTechnologyType;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Commands.UpdateProgrammingTechnologyType;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Dtos;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Models;
+using Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Queries.GetListProgrammingTechnologyType;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodlama.io.Devs.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingTechnologyTypesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingTechnologyTypeCommand createProgrammingTechnologyTypeCommand)
+        {
+            CreatedProgrammingTechnologyTypeDto result = await Mediator.Send(createProgrammingTechnologyTypeCommand);
+            return Created("", result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateProgrammingTechnologyTypeCommand updateProgrammingTechnologyTypeCommand)
+        {
+            UpdatedProgrammingTechnologyTypeDto result = await Mediator.Send(updateProgrammingTechnologyTypeCommand);
+            return Ok(result);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete([FromRoute] DeleteProgrammingTechnologyTypeCommand deleteProgrammingTechnologyTypeCommand)
+        {
+            DeletedProgrammingTechnologyTypeDto result = await Mediator.Send(deleteProgrammingTechnologyTypeCommand);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListProgrammingTechnologyTypeQuery getListProgrammingTechnologyTypeQuery = new() { PageRequest = pageRequest };
+            ProgrammingTechnologyTypeListModel result = await Mediator.Send(getListProgrammingTechnologyTypeQuery);
+
+            return Ok(result);
+        }
+    }
+}
