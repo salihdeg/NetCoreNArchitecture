147c9f9edb6896f7946e41a23d1415465676a4e4
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-09 04:53:47 +0300

    Some Seed Data and New Entities Added.

--- src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Context/BaseDbContext.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Persistence/Context/BaseDbContext.cs
@@ -8,6 +8,8 @@ namespace Kodlama.io.Devs.Persistence.Context
     {
         public IConfiguration Configuration { get; set; }
         public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+        public DbSet<ProgrammingTechnology> programmingTechnologies { get; set; }
+        public DbSet<ProgrammingTechnologyType> ProgrammingTechnologyTypes { get; set; }
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -28,10 +30,35 @@ namespace Kodlama.io.Devs.Persistence.Context
                 p.ToTable("ProgrammingLanguages").HasKey(k => k.Id);
                 p.Property(p => p.Id).HasColumnName("Id");
                 p.Property(p => p.Name).HasColumnName("Name");
+                p.HasMany(p => p.ProgrammingTechnologies);
             });
 
-            ProgrammingLanguage[] programmingLanguageSeedData = { new(1, "c#"), new(2, "Java") };
+            modelBuilder.Entity<ProgrammingTechnologyType>(p =>
+            {
+                p.ToTable("ProgrammingTechnologyTypes").HasKey(k => k.Id);
+                p.Property(p => p.Id).HasColumnName("Id");
+                p.Property(p => p.Name).HasColumnName("Name");
+                p.HasMany(p => p.ProgrammingTechnologies);
+            });
+
+            modelBuilder.Entity<ProgrammingTechnology>(p =>
+            {
+                p.ToTable("ProgrammingTechnologies").HasKey(k => k.Id);
+                p.Property(p => p.Id).HasColumnName("Id");
+                p.Property(p => p.ProgrammingLanguageId).HasColumnName("ProgrammingLanguageId");
+                p.Property(p => p.ProgrammingTechnologyTypeId).HasColumnName("ProgrammingTechnologyTypeId");
+                p.Property(p => p.Name).HasColumnName("Name");
+                p.HasOne(p => p.ProgrammingLanguage);
+                p.HasOne(p => p.ProgrammingTechnologyType);
+            });
+
+            ProgrammingLanguage[] programmingLanguageSeedData = { new(1, "c#"), new(2, "Java"), new(3, "Rust") };
+            ProgrammingTechnologyType[] programmingTechnologyTypeSeedData = { new(1, "Framework"), new(2, "Library") };
+            ProgrammingTechnology[] programmingTechnologies = { new(1, 1, 1, "ASP.NET"), new(2, 2, 2, "Google Guava"), new(3, 3, 1, "GGEZ") };
+
             modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageSeedData);
+            modelBuilder.Entity<ProgrammingTechnologyType>().HasData(programmingTechnologyTypeSeedData);
+            modelBuilder.Entity<ProgrammingTechnology>().HasData(programmingTechnologies);
         }
     }
 }
