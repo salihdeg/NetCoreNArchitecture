7b1b41e777c55eba3bc81dd60cdc1a3ca7681c59
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-09 05:10:42 +0300

    Business Rules Added for Programming Technologies

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingTechnologies/Rules/ProgrammingTechnologyBusinessRules.cs
@@ -0,0 +1,35 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingTechnologies.Rules
+{
+    public class ProgrammingTechnologyBusinessRules
+    {
+        private readonly IProgrammingTechnologyRepository _programmingTechnologyRepository;
+
+        public ProgrammingTechnologyBusinessRules(IProgrammingTechnologyRepository programmingTechnologyRepository)
+        {
+            _programmingTechnologyRepository = programmingTechnologyRepository;
+        }
+
+        public async Task ProgrammingTechnologyNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<ProgrammingTechnology> result = await _programmingTechnologyRepository.GetListAsync(ptt => ptt.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming Technology Exist!");
+        }
+
+        public async Task<ProgrammingTechnology> ProgrammingTechnologyMustBeExist(int id)
+        {
+            ProgrammingTechnology result = await _programmingTechnologyRepository.GetAsync(ptt => ptt.Id == id, enableTracking: false);
+            if (result == null) throw new BusinessException("Programming Technology Does Not Exist!");
+            else return result;
+        }
+
+        public void ProgrammingTechnologyMustBeExist(ProgrammingTechnology programmingTechnology)
+        {
+            if(programmingTechnology == null) throw new BusinessException("Programming Technology Does Not Exist!");
+        }
+    }
+}
