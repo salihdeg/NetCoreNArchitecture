611664f4869f121fa494fe53ac6a717cdb8b5ace
Salih Değirmenci <salihdegirmenci99@gmail.com>
2022-09-09 05:11:19 +0300

    Business Rules Added for Programming Technology Types

---
+++ src/projects/kodlama.io.devs/Kodlama.io.Devs.Application/Features/ProgrammingTechnologyTypes/Rules/ProgrammingTechnologyTypeBusinessRules.cs
@@ -0,0 +1,29 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingTechnologyTypes.Rules
+{
+    public class ProgrammingTechnologyTypeBusinessRules
+    {
+        private readonly IProgrammingTechnologyTypeRepository _programmingTechnologyTypeRepository;
+
+        public ProgrammingTechnologyTypeBusinessRules(IProgrammingTechnologyTypeRepository programmingTechnologyTypeRepository)
+        {
+            _programmingTechnologyTypeRepository = programmingTechnologyTypeRepository;
+        }
+
+        public async Task ProgrammingTechnologyTypeNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<ProgrammingTechnologyType> result = await _programmingTechnologyTypeRepository.GetListAsync(ptt => ptt.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming Technology Type Exist!");
+        }
+
+        public async Task ProgrammingTechnologyTypeMustBeExist(int id)
+        {
+            ProgrammingTechnologyType result = await _programmingTechnologyTypeRepository.GetAsync(ptt => ptt.Id == id);
+            if (result == null) throw new BusinessException("Programming Technology Type Does Not Exist!");
+        }
+    }
+}
